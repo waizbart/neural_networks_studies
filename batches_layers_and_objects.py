99f8e82ca869468eaca0756774fa65807ef3ea4d
Guilherme Waizbart <guilhermewaizbart@gmail.com>
2023-04-04 16:57:14 -0300

    POO

---
+++ batches_layers_and_objects.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+np.random.seed(0)
+
+X = [[1, 2, 3, 2.5],
+     [2, 5, -1, 2],
+     [-1.5, 2.7, 3.3, -0.8]]
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+layer1 = Layer_Dense(4, 20)
+layer2 = Layer_Dense(20, 2)
+
+layer1.forward(X)
+layer2.forward(layer1.output)
+
+print(layer2.output)
--- numpy_python_neural_network.py
+++ numpy_python_neural_network.py
@@ -1,24 +1,22 @@
 import numpy as np
 
-inputs = [1, 2, 3, 2.5]
+inputs = [[1, 2, 3, 2.5],
+          [2, 5, -1, 2],
+          [-1.5, 2.7, 3.3, -0.8]]
 
 weights = [[0.2, 0.8, -0.5, 1.0], 
            [0.5, -0.91, 0.26, -0.5], 
            [-0.26, -0.27, 0.17, 0.87]]
 
-biases = [
-    2, 3, 0.5
-]
+biases = [2, 3, 0.5]
 
-layer_outputs = []
+weights2 = [[0.1, -0.14, 0.5], 
+           [-0.5, 0.12, -0.33], 
+           [-0.44, 0.73, -0.13]]
 
-for neuron_weights, neuron_bias in zip(weights, biases):
-    neuron_output = 0
-    for n_input, weight in zip(inputs, neuron_weights):
-        neuron_output += n_input * weight
-    neuron_output += neuron_bias
-    layer_outputs.append(neuron_output)
+biases2 = [-1, 2, -0.5]
 
-output = np.dot(weights, inputs) + biases
+layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
+layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
 
-print(layer_outputs, output)
+print(layer2_outputs)
